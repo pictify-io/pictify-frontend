0084d81a07a910a9bcb91343afa712e0afd51616
suyash-thakur <500067151@stu.upes.ac.in>
2024-03-10 02:35:08 +0530

    created sitemap

---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,20 @@
+export async function GET() {
+  return new Response(
+    `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <url>
+          <loc>http://pictify.io/</loc>
+          <lastmod>2024-03-07</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.9</priority>
+       </url>
+    </urlset>
+    `.trim(),
+    {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    }
+  );
+}
