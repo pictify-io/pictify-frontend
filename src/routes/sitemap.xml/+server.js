4f99051149c9d1042a1f8212ca2d22b6cef7f129
Suyash Thakur <47927368+suyash-thakur@users.noreply.github.com>
2024-03-11 23:41:54 +0530

    few changes for seo (#2)
    * few changes for seo
    
    * minor changes

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -4,9 +4,9 @@ export async function GET() {
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
        <url>
-          <loc>http://pictify.io/</loc>
-          <lastmod>2024-03-07</lastmod>
-          <changefreq>monthly</changefreq>
+          <loc>https://pictify.io/</loc>
+          <lastmod>2024-03-11</lastmod>
+          <changefreq>daily</changefreq>
           <priority>0.9</priority>
        </url>
     </urlset>
