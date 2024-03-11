2282573894a2b8ab6a5347d043cda79e30e787b2
suyash-thakur <500067151@stu.upes.ac.in>
2024-03-11 23:34:46 +0530

    minor changes

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -5,8 +5,8 @@ export async function GET() {
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
        <url>
           <loc>https://pictify.io/</loc>
-          <lastmod>2024-03-07</lastmod>
-          <changefreq>monthly</changefreq>
+          <lastmod>2024-03-11</lastmod>
+          <changefreq>daily</changefreq>
           <priority>0.9</priority>
        </url>
     </urlset>
