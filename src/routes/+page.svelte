9c8b539fd3774b32d748b00199992256199aa6ee
suyash-thakur <500067151@stu.upes.ac.in>
2024-06-20 01:19:39 +0530

    update meta description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,7 @@
 
 <svelte:head>
     <title>Pictify.io: Free HTML to Image and Gif API</title>
-    <meta name="description" content="Pictify.io is a free HTML to Image and Gif API">
+    <meta name="description" content="Convert HTML to Image or Gif using free HTML to Image API by Pictify.io. Perfect for developers to generate images from HTML at scale with ease.">
     <meta name="keywords" content="HTML to image, HTML to gif, HTML to image API, HTML to gif API, HTML CSS to Image API, HTML CSS to Gif API, Gif, API, Pictify.io, Pictify, Pictify API, Pictify.io API, Pictify.io Gif API, Pictify.io HTML to CSS API, Pictify.io HTML to CSS and Gif API, Pictify.io HTML to CSS and Gif conversion API, Pictify.io HTML to CSS and Gif conversion">
     <meta name="author" content="Pictify.io">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -46,7 +46,7 @@
           "name": "Pictify",
           "url": "https://pictify.io",
           "image": "https://res.cloudinary.com/diroilukd/image/upload/v1709358454/P_jeay4c.png",
-          "description": "Pictify.io is a free HTML to Image and Gif API",
+          "description": "Convert HTML to Image or Gif using free HTML to Image API by Pictify.io. Perfect for developers to generate images from HTML at scale with ease.",
           "brand": {
             "@type": "Brand",
             "name": "Pictify"
