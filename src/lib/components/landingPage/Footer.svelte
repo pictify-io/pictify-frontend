fa44e5ec275a35b27f8031b126c9b838a29f0987
suyash-thakur <500067151@stu.upes.ac.in>
2024-07-18 02:58:02 +0530

    add terms and privacy page

--- src/lib/components/landingPage/Footer.svelte
+++ src/lib/components/landingPage/Footer.svelte
@@ -1,20 +1,13 @@
 <footer
-	class="sm:h-10 h-auto z-20 border-t-[3px] sm:pb-0 pb-3 w-full border-gray-900 sm:space-y-0 space-y-3 flex sm:flex-row flex-col items-center justify-center flex-shrink-0"
+    class="h-auto z-20 border-t-[3px] pb-3 w-full border-gray-900 sm:space-y-0 space-y-3 flex sm:flex-row flex-col items-center justify-center flex-shrink-0"
 >
-	<!-- <p class="text-sm opacity-70 sm:mt-0 mt-3">Keep posted on our social channels <span class="sm:inline hidden">→</span></p> -->
-	<!-- <div class="flex items-center justify-center space-x-1 ml-4">
-        <a href="#_" class="p-1">
-            <img src="https://cdn.devdojo.com/images/january2023/twitter.png" class="w-7 translate-y-px h-auto">
-        </a>
-        <a href="#_" class="p-1">
-            <img src="https://cdn.devdojo.com/images/january2023/facebook.png" class="w-7 h-auto">
-        </a>
-        <a href="#_" class="p-1">
-            <img src="https://cdn.devdojo.com/images/january2023/youtube.png" class="w-7 h-auto">
-        </a>
-        <a href="#_" class="p-1 pl-1.5">
-            <img src="https://cdn.devdojo.com/images/january2023/linkedin.png" class="w-7 h-auto">
-        </a>
-    </div> -->
-	<p class="text-sm opacity-70 mt-3">© 2024 Pictify.io</p>
+<div class="flex-col items-center justify-center">
+    <!-- Content omitted for brevity -->
+    <div class="mt-3 flex justify-center space-x-4">
+        <a href="/terms" class="text-sm opacity-70 hover:opacity-100">Terms</a>
+        <a href="/privacy" class="text-sm opacity-70 hover:opacity-100">Privacy</a>
+    </div>
+    <p class="text-sm opacity-70 mt-3">© 2024 Pictify.io</p>
+
+</div>
 </footer>
