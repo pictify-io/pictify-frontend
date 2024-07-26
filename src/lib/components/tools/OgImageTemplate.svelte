8560879fca45c65072053c84e4b728aab8ad50f2
suyash-thakur <500067151@stu.upes.ac.in>
2024-07-27 04:38:07 +0530

    function to update the template

--- src/lib/components/tools/OgImageTemplate.svelte
+++ src/lib/components/tools/OgImageTemplate.svelte
@@ -1,22 +1,15 @@
 <script>
   export let html = '';
+  export let width = 1200;
+  export let height = 600;
+  export let scale = 0.3;
 </script>
 
-<div>
+<div style="width: {width * scale}px; height: {height * scale}px;">
  <iframe
   title="og-image-template"
    srcdoc={html}
    scale="0"
-    class="scaled-iframe"
+   style="width: {width}px; height: {height}px; transform: scale({scale}); transform-origin: top left;" 
   ></iframe>
 </div>
-
-<style>
-  .scaled-iframe {
-    width: 900px; /* Adjust as needed */
-    height: 400px; /* Adjust as needed */   
-    scale: 0.5;
-    transform-origin: top left;
-    border: 0; /* Optional: removes the border */
-  }
-</style>
--- src/routes/tools/og-image-generator/+page.svelte
+++ src/routes/tools/og-image-generator/+page.svelte
@@ -2,10 +2,57 @@
   import Nav from '$lib/components/landingPage/Nav.svelte';
   import Footer from '$lib/components/landingPage/Footer.svelte';
   import OgImageTemplate from '$lib/components/tools/OgImageTemplate.svelte';
-    import {getTemplate, getWebsiteInfo} from '../../../api/tools/og-image';
+  import {getTemplate, getWebsiteInfo} from '../../../api/tools/og-image';
   import { onMount } from 'svelte';
+  import ColorPicker, { ChromeVariant } from 'svelte-awesome-color-picker';
 
   let url = '';
+  let selectedTemplate = '';
+  let isFetchingWebsiteInfo = false;
+  let websiteInfo = {
+    "heading": "Simple and Robust\nHTML To Image API",
+    "subHeading": "Convert HTML to Image or Gif using free HTML to Image API by Pictify.io. Perfect for developers to generate images from HTML at scale with ease.",
+    "logo": "https://pictify.io/_app/immutable/assets/POM.DnFnDFPx.svg",
+    "colors": [
+        [
+            251,
+            250,
+            249
+        ],
+        [
+            18,
+            21,
+            20
+        ],
+        [
+            153,
+            151,
+            149
+        ],
+        [
+            235,
+            74,
+            96
+        ],
+        [
+            153,
+            135,
+            111
+        ]
+    ],
+    "headingColor": "rgb(0, 0, 0)"
+};
+
+let backgroundColorRgb = websiteInfo?.colors ? {r: websiteInfo.colors[0][0], g: websiteInfo.colors[0][1], b: websiteInfo.colors[0][2]} : {r: 255, g: 255, b: 255};
+let headingColorRgb = websiteInfo?.colors ? {r: websiteInfo.colors[1][0], g: websiteInfo.colors[1][1], b: websiteInfo.colors[1][2]} : {r: 0, g: 0, b: 0};
+let subHeadingColorRgb = websiteInfo?.colors ? {r: websiteInfo.colors[2][0], g: websiteInfo.colors[2][1], b: websiteInfo.colors[2][2]} : {r: 0, g: 0, b: 0};
+
+
+
+let ogImageTemplateWrapper;
+ $:previewWidth =  ogImageTemplateWrapper ? (ogImageTemplateWrapper.offsetWidth * 2) - 70 : 1200;
+$:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  : 600;
+
   const isValidUrl = (url) => {
     const urlRegex = new RegExp(
       '^(https?:\\/\\/)?' + // protocol
@@ -25,12 +72,16 @@
   };
 
   const submitUrl = async (url) => {
-    console.log(url);
     if (!isValidUrl(url)) {
       return;
     }
-    const websiteInfo = await getWebsiteInfo(url);
-    console.log(websiteInfo);
+    isFetchingWebsiteInfo = true;
+    websiteInfo = await getWebsiteInfo(url);
+    isFetchingWebsiteInfo = false;
+    backgroundColorRgb = {r: websiteInfo.colors[0][0], g: websiteInfo.colors[0][1], b: websiteInfo.colors[0][2]};
+    headingColorRgb = {r: websiteInfo.colors[1][0], g: websiteInfo.colors[1][1], b: websiteInfo.colors[1][2]};
+    subHeadingColorRgb = {r: websiteInfo.colors[2][0], g: websiteInfo.colors[2][1], b: websiteInfo.colors[2][2]};
+    await updateHTML(selectedTemplate);
   };
 
   const templateNames = ['template-1'];
@@ -41,7 +92,33 @@
       const template = await getTemplate(name);
       return template;
     }));
+
+    selectedTemplate = templates[0];
   });
+
+  const updateHTML = (html) => {
+    const iframe = ogImageTemplateWrapper.querySelector('iframe');
+
+    const document = iframe.contentWindow.document;
+    const heading= document.querySelector('#template-heading');
+    const subHeading= document.querySelector('#template-subheading');
+    const logo= document.querySelector('#template-logo');
+    heading.innerHTML = websiteInfo.heading;
+    subHeading.innerHTML = websiteInfo.subHeading;
+    if(logo) {
+      logo.src = websiteInfo.logo;
+    }
+   
+    document.documentElement.style.setProperty('--primary-color', `rgb(${backgroundColorRgb.r}, ${backgroundColorRgb.g}, ${backgroundColorRgb.b})`);
+    document.documentElement.style.setProperty('--secondary-color', `rgb(${headingColorRgb.r}, ${headingColorRgb.g}, ${headingColorRgb.b})`);
+    document.documentElement.style.setProperty('--tertiary-color', `rgb(${subHeadingColorRgb.r}, ${subHeadingColorRgb.g}, ${subHeadingColorRgb.b})`);
+
+
+
+    html = document.documentElement.outerHTML;
+  };
+
+
 </script>
 
 
@@ -101,35 +178,170 @@
   Generate beautiful Open Graph images for your website or blog with our OG Image Generator.
 </h2>
   </main>
-  <div class="mt-20 w-full max-w-4xl m-auto">
-    <input
-      on:input={(e) => {
-        if (isValidUrl(e.target.value)) {
-          e.target.classList.remove('border-red-500');
-        } else {
-          e.target.classList.add('border-red-500');
-        }
-      }}
-      bind:value={url}
-      type="text"
-      class="w-full border-[3px] border-gray-900 placeholder-gray-600 text-lg font-medium focus:outline-none py-3.5 px-6 rounded"
-      placeholder="Enter you website URL"
-    />
+  <div class="px-6 md:px-0 mt-20 w-full max-w-4xl m-auto">
+    <div class="w-full flex flex-col gap-4 md:flex-row">
+      <div class="flex-grow">
+        <input
+        on:input={(e) => {
+          if (isValidUrl(e.target.value)) {
+            e.target.classList.remove('border-red-500');
+          } else {
+            e.target.classList.add('border-red-500');
+          }
+        }}
+        bind:value={url}
+        type="text"
+        class="w-full border-[3px] border-gray-900 placeholder-gray-600 text-lg font-medium focus:outline-none py-3.5 px-6 rounded"
+        placeholder="Enter you website URL"
+      />
+      </div>
+   <div>
     <button
-      on:click={(event) => { console.log("called"); event.preventDefault(); submitUrl(url); }}
-      class="w-full bg-[#FE4A60] text-white text-lg font-medium py-3.5 rounded mt-4"
-    >
-      Generate
-    </button>
-    <div class="my-10 text-lg font-bold">
+    on:click={(event) => { console.log("called"); event.preventDefault(); submitUrl(url); }}
+    class="w-full px-6 text-gray-900 bg-[#ffc480] border-[3px] border-gray-900 text-lg font-medium py-3.5 rounded mt-4 md:mt-0"
+  >
+    Fetch Website Info
+  </button>
+   </div>
+  
+  </div>
+    {#if isFetchingWebsiteInfo}
+    <div class="text-lg font-bold mt-10 text-gray-600">
+      Fetching website info...
+      <div class="mt-5 w-full bg-gray-200 rounded-full h-3 dark:bg-gray-300">
+        <div class="bg-gray-900 h-3 rounded-full" style="width: 0%; animation: loading 3s forwards;"></div>
+      </div>
+    </div>
+    {/if}
+
+    {#if websiteInfo && selectedTemplate}
+    <div class="flex:col mt-10 w-full gap-4 justify-between p-4 bg-slate-200 border-[3px] border-gray-900 rounded" bind:this={ogImageTemplateWrapper}>
+      <div class="w-full flex justify-center">
+        <OgImageTemplate html={selectedTemplate} width={previewWidth} height={previewHeight} scale=0.5 />
+      </div>
+      <div class="w-full mt-8">
+
+        <div class="mt-4">
+          <div class="font-semibold text-lg">
+            Logo
+          </div>
+          <div class="mt-2 flex w-full relative items-center">
+            {#if websiteInfo.logo}
+            <img src={websiteInfo.logo} class="w-[150px]" alt="input-logo"/>
+            {/if}
+            <input
+              type="file"
+              class="opacity-0 absolute inset-0 w-full cursor-pointer"
+              accept="image/*"
+              placeholder="Upload Logo"
+            />
+            <label for="fileInput" class="ml-4 p-2 h-fit rounded cursor-pointer bg-gray-700 text-white">
+              Upload Logo
+            </label>
+          </div>
+        </div>
+        <div class="mt-6">
+          <div class="font-semibold text-lg">
+            Heading
+          </div>
+          <input
+            type="text"
+            class="w-full mt-2 border-[3px] border-gray-900 placeholder-gray-600  focus:outline-none p-2 rounded"
+            placeholder="Heading"
+            value={websiteInfo.heading}
+          />
+
+          <div class="mt-6 font-semibold text-lg">
+            Sub-Heading
+          </div>
+          <textarea
+            class="w-full mt-2 border-[3px] border-gray-900 placeholder-gray-600 focus:outline-none p-2 rounded"
+            placeholder="Sub-Heading"
+            rows="4"
+            value={websiteInfo.subHeading}
+          ></textarea>
+        </div>
+        <div class="mt-4">
+          <div class="flex flex-col md:flex-row gap-6">
+            <div class="mt-4">
+              <div class="font-semibold text-lg mb-4">
+                Background Color
+              </div>
+              <ColorPicker
+                bind:rgb={backgroundColorRgb}
+                isDialog={true}
+              />
+            </div>
+  
+            <div class="mt-4">
+              <div class="font-semibold text-lg mb-4">
+                Heading Color
+              </div>
+              <ColorPicker
+                bind:rgb={headingColorRgb}
+                isDialog={true}
+              />
+            </div>
+  
+            <div class="mt-4">
+              <div class="font-semibold text-lg mb-4">
+                Sub-Heading Color
+              </div>
+              <ColorPicker
+                bind:rgb={subHeadingColorRgb}
+                isDialog={true}
+              />
+            </div>
+          </div>
+
+        </div>
+
+        <div class="mt-8">
+          <button
+            class="w-full text-gray-900 bg-[#ffc480] border-[3px] border-gray-900 text-lg font-medium py-3.5 rounded"
+          >
+            Generate Image
+          </button>
+        </div>
+      </div>
+    </div>
+    {/if}
+
+    <div class="mt-20 mb-10 text-2xl font-bold">
       Templates
     </div>
+ 
+    <div class="flex flex-col md:flex-row gap-2 mb-10">
     {#each templates as template}
-      <div>
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <div
+        class="{selectedTemplate === template ? 'border-[3px] border-[#5cf1a4]' : ''} cursor-pointer relative w-fit"
+        on:click={() => selectedTemplate = template}
+      >
+      <div class="absolute inset-0 z-0">
+      {#if selectedTemplate === template}
+        <div class="m-2">
+          ✅
+        </div>
+      {/if}
+      </div>
         <OgImageTemplate html={template} />
       </div>
     {/each}
   </div>
-
+</div>
   <Footer />
+
+  <style>
+    @keyframes loading {
+      0% {
+        width: 0%;
+      }
+      100% {
+        width: 100%;
+      }
+    }
+  </style>
 </section>
+
