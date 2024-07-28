b099bd356d7e816f5b42b3462b3fea2626c967db
suyash-thakur <500067151@stu.upes.ac.in>
2024-07-28 13:27:29 +0530

    fix template change functionality

--- src/routes/tools/og-image-generator/+page.svelte
+++ src/routes/tools/og-image-generator/+page.svelte
@@ -3,45 +3,54 @@
   import Footer from '$lib/components/landingPage/Footer.svelte';
   import OgImageTemplate from '$lib/components/tools/OgImageTemplate.svelte';
   import {getTemplate, getWebsiteInfo} from '../../../api/tools/og-image';
+  import { createImagePublic } from '../../../api/image.js';
   import { onMount } from 'svelte';
-  import ColorPicker, { ChromeVariant } from 'svelte-awesome-color-picker';
+  import { toast } from '../../../store/toast.store';
+  import ColorPicker from 'svelte-awesome-color-picker';
+
+  const popularFontsLinks = [
+  'https://fonts.googleapis.com/css2?family=Arial:wght@100;200;300;400;500;600;700;800;900&display=swap',
+	'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap',
+  'https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap',
+  'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap',
+	'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap',
+	'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap',
+	'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap',
+	'https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap',
+	'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap',
+  'https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap',
+	'https://fonts.googleapis.com/css2?family=DynaPuff:wght@400..700&display=swap'
+];
+
+const popularFonts = [
+  {id:'Arial', name: 'Arial', className: 'arial'},
+	{ id: 'Roboto', name: 'Roboto', className: 'roboto' },
+	{ id: 'Roboto Condensed', name: 'Roboto Condensed', className: 'roboto-condensed' },
+	{ id: 'Open Sans', name: 'Open Sans', className: 'open-sans' },
+	{ id: 'Montserrat', name: 'Montserrat', className: 'montserrat' },
+	{ id: 'Poppins', name: 'Poppins', className: 'poppins' },
+	{ id: 'Source Sans Pro', name: 'Source Sans Pro', className: 'source-sans-pro' },
+	{ id: 'Oswald', name: 'Oswald', className: 'oswald' },
+	{ id: 'Inter', name: 'Inter', className: 'inter' },
+	{ id: 'Manrope', name: 'Manrope', className: 'manrope' },
+	{ id: 'DynaPuff', name: 'DynaPuff', className: 'dynapuff' }
+];
+
+const combinedFonts = popularFonts.map((font, index) => ({
+  ...font,
+  link: popularFontsLinks[index]
+}));
 
   let url = '';
   let selectedTemplate = '';
   let isFetchingWebsiteInfo = false;
-  let websiteInfo = {
-    "heading": "Simple and Robust\nHTML To Image API",
-    "subHeading": "Convert HTML to Image or Gif using free HTML to Image API by Pictify.io. Perfect for developers to generate images from HTML at scale with ease.",
-    "logo": "https://pictify.io/_app/immutable/assets/POM.DnFnDFPx.svg",
-    "colors": [
-        [
-            251,
-            250,
-            249
-        ],
-        [
-            18,
-            21,
-            20
-        ],
-        [
-            153,
-            151,
-            149
-        ],
-        [
-            235,
-            74,
-            96
-        ],
-        [
-            153,
-            135,
-            111
-        ]
-    ],
-    "headingColor": "rgb(0, 0, 0)"
-};
+  let selectedFont = combinedFonts[0];
+  let logoWidth = 150;
+
+  let imageUrl = '';
+  let isImageGenerating = false;
+
+  let websiteInfo;
 
 let backgroundColorRgb = websiteInfo?.colors ? {r: websiteInfo.colors[0][0], g: websiteInfo.colors[0][1], b: websiteInfo.colors[0][2]} : {r: 255, g: 255, b: 255};
 let headingColorRgb = websiteInfo?.colors ? {r: websiteInfo.colors[1][0], g: websiteInfo.colors[1][1], b: websiteInfo.colors[1][2]} : {r: 0, g: 0, b: 0};
@@ -81,6 +90,19 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
     backgroundColorRgb = {r: websiteInfo.colors[0][0], g: websiteInfo.colors[0][1], b: websiteInfo.colors[0][2]};
     headingColorRgb = {r: websiteInfo.colors[1][0], g: websiteInfo.colors[1][1], b: websiteInfo.colors[1][2]};
     subHeadingColorRgb = {r: websiteInfo.colors[2][0], g: websiteInfo.colors[2][1], b: websiteInfo.colors[2][2]};
+    
+    
+    while(!ogImageTemplateWrapper) {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 100);
+      });
+    }
+
+    const iframe = ogImageTemplateWrapper.querySelector('iframe');
+    await new Promise((resolve) => {
+      iframe.onload = resolve;
+    });
+    
     await updateHTML(selectedTemplate);
   };
 
@@ -97,26 +119,109 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
   });
 
   const updateHTML = (html) => {
+    console.log('updateHTML');
+    console.log(html);
     const iframe = ogImageTemplateWrapper.querySelector('iframe');
 
     const document = iframe.contentWindow.document;
     const heading= document.querySelector('#template-heading');
     const subHeading= document.querySelector('#template-subheading');
     const logo= document.querySelector('#template-logo');
-    heading.innerHTML = websiteInfo.heading;
-    subHeading.innerHTML = websiteInfo.subHeading;
+    heading.innerHTML = websiteInfo.heading || '';
+    subHeading.innerHTML = websiteInfo.subHeading || '';
+
     if(logo) {
+      if(websiteInfo.logo && websiteInfo.logo.startsWith('<svg')) {
+       
+        logo.outerHTML = websiteInfo.logo;
+        logo.id = 'template-logo';
+        websiteInfo.logo = URL.createObjectURL(new Blob([websiteInfo.logo], {type: 'image/svg+xml'}));
+      } else {
       logo.src = websiteInfo.logo;
+      logo.width = logoWidth;
+      }
     }
    
     document.documentElement.style.setProperty('--primary-color', `rgb(${backgroundColorRgb.r}, ${backgroundColorRgb.g}, ${backgroundColorRgb.b})`);
     document.documentElement.style.setProperty('--secondary-color', `rgb(${headingColorRgb.r}, ${headingColorRgb.g}, ${headingColorRgb.b})`);
     document.documentElement.style.setProperty('--tertiary-color', `rgb(${subHeadingColorRgb.r}, ${subHeadingColorRgb.g}, ${subHeadingColorRgb.b})`);
 
+    // Update Font
+    const fontLink = document.createElement('link');
+    fontLink.rel = 'stylesheet';
+    fontLink.href = selectedFont.link;
 
+    document.head.appendChild(fontLink);
+
+    delete document.documentElement.style.fontFamily;
+    document.documentElement.style.fontFamily = selectedFont.id;
 
     html = document.documentElement.outerHTML;
   };
+
+  const updateHeading = (event) => {
+    websiteInfo.heading = event.target.value;
+    updateHTML(selectedTemplate);
+  };
+
+  const updateSubHeading = (event) => {
+    websiteInfo.subHeading = event.target.value;
+    updateHTML(selectedTemplate);
+  };
+
+  const updateLogo = (event) => {
+    websiteInfo.logo = URL.createObjectURL(event.target.files[0]);
+    updateHTML(selectedTemplate);
+  };
+
+  const updateBackgroundColor = (event) => {
+    const rgb = event.detail.rgb;
+    backgroundColorRgb = rgb;
+    updateHTML(selectedTemplate);
+  };
+
+  const updateHeadingColor = (event) => {
+    const rgb = event.detail.rgb;
+    headingColorRgb = rgb;
+    updateHTML(selectedTemplate);
+  };
+
+  function copyToClipboard(text) {
+		navigator.clipboard.writeText(text).then(() => {
+			toast.set({ message: 'Copied to clipboard !!', duration: 1500 });
+		});
+	}
+
+  // const updateSubHeadingColor = (event) => {
+  //   const rgb = event.detail.rgb;
+  //   subHeadingColorRgb = rgb;
+  //   updateHTML(selectedTemplate);
+  // };
+
+  const updateFont = (font) => {
+    selectedFont = font;
+    updateHTML(selectedTemplate);
+  };
+
+  const updateLogoWidth = (event) => {
+    logoWidth = event.target.value;
+    updateHTML(selectedTemplate);
+  };
+
+  const generateImage = async () => {
+    isImageGenerating = true;
+    const iframe = ogImageTemplateWrapper.querySelector('iframe');
+    const document = iframe.contentWindow.document;
+    const html = document.documentElement.outerHTML;
+    const {image} = await createImagePublic({
+      html,
+      width: previewWidth,
+      height: previewHeight
+    });
+    imageUrl = image.url;
+    isImageGenerating = false;
+  };
+
 
 
 </script>
@@ -197,7 +302,7 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
       </div>
    <div>
     <button
-    on:click={(event) => { console.log("called"); event.preventDefault(); submitUrl(url); }}
+    on:click={(event) => {  event.preventDefault(); submitUrl(url); }}
     class="w-full px-6 text-gray-900 bg-[#ffc480] border-[3px] border-gray-900 text-lg font-medium py-3.5 rounded mt-4 md:mt-0"
   >
     Fetch Website Info
@@ -215,8 +320,8 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
     {/if}
 
     {#if websiteInfo && selectedTemplate}
-    <div class="flex:col mt-10 w-full gap-4 justify-between p-4 bg-slate-200 border-[3px] border-gray-900 rounded" bind:this={ogImageTemplateWrapper}>
-      <div class="w-full flex justify-center">
+    <div class="flex-col mt-10 w-full gap-4 justify-between p-4 bg-slate-200 border-[3px] border-gray-900 rounded" bind:this={ogImageTemplateWrapper}>
+      <div class="w-full flex justify-center ">
         <OgImageTemplate html={selectedTemplate} width={previewWidth} height={previewHeight} scale=0.5 />
       </div>
       <div class="w-full mt-8">
@@ -234,12 +339,18 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
               class="opacity-0 absolute inset-0 w-full cursor-pointer"
               accept="image/*"
               placeholder="Upload Logo"
+              on:change={updateLogo}
             />
             <label for="fileInput" class="ml-4 p-2 h-fit rounded cursor-pointer bg-gray-700 text-white">
               Upload Logo
             </label>
           </div>
-        </div>
+          <div class="mt-4 text-sm text-gray-600">
+            <input type="number" class="w-20 border-[3px] border-gray-900 placeholder-gray-600 focus:outline-none p-2 rounded" value={logoWidth} on:input={updateLogoWidth} />
+            <span class="ml-2">Logo Width</span>
+          </div>
+          </div>
+
         <div class="mt-6">
           <div class="font-semibold text-lg">
             Heading
@@ -249,6 +360,7 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
             class="w-full mt-2 border-[3px] border-gray-900 placeholder-gray-600  focus:outline-none p-2 rounded"
             placeholder="Heading"
             value={websiteInfo.heading}
+            on:input={updateHeading}
           />
 
           <div class="mt-6 font-semibold text-lg">
@@ -259,10 +371,24 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
             placeholder="Sub-Heading"
             rows="4"
             value={websiteInfo.subHeading}
+            on:input={updateSubHeading}
           ></textarea>
         </div>
-        <div class="mt-4">
-          <div class="flex flex-col md:flex-row gap-6">
+        <div class="mt-4 relative">
+          <div class="font-semibold text-lg mb-4">
+            Font
+          </div>
+          <select
+            class="w-full border-[3px] border-gray-900 placeholder-gray-600 text-lg font-medium focus:outline-none p-2 rounded"
+            on:change={(e) => updateFont(combinedFonts[e.target.selectedIndex])}
+          >
+            {#each combinedFonts as font}
+            <option value={font.id}>{font.name}</option>
+            {/each}
+          </select>
+        </div>
+        <div class="mt-4 relative">
+          <div class="flex flex-col md:flex-row gap-6 z-25">
             <div class="mt-4">
               <div class="font-semibold text-lg mb-4">
                 Background Color
@@ -270,35 +396,76 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
               <ColorPicker
                 bind:rgb={backgroundColorRgb}
                 isDialog={true}
+                on:input={updateBackgroundColor}
               />
             </div>
   
             <div class="mt-4">
               <div class="font-semibold text-lg mb-4">
-                Heading Color
+                Text Color
               </div>
               <ColorPicker
                 bind:rgb={headingColorRgb}
                 isDialog={true}
+                on:input={updateHeadingColor}
               />
             </div>
   
-            <div class="mt-4">
+            <!-- <div class="mt-4">
               <div class="font-semibold text-lg mb-4">
                 Sub-Heading Color
               </div>
               <ColorPicker
                 bind:rgb={subHeadingColorRgb}
                 isDialog={true}
+                on:input={updateSubHeadingColor}
               />
-            </div>
+            </div> -->
           </div>
 
         </div>
+        {#if imageUrl}
+        <div class="mt-8">
+          <div class="font-semibold text-lg">
+            Image URL
+          </div>
+          <div class="flex">
+            <div>
+            <a href={imageUrl} download="og-image.jpg" class=" text-lg font-medium py-3.5 ">
+              {imageUrl}
+             </a>
+            </div>
 
+            <button
+            on:click={() => {
+              copyToClipboard(imageUrl);
+            }}
+            class="ml-4 text-xs bg-black hover:bg-black text-white py-1 px-2 rounded"
+          >
+            <div class="flex justify-between items-center">
+              <div>Copy URL</div>
+            </div>
+          </button>
+          </div>
+       
+          <img src={imageUrl} class="w-[150px] mt-4" alt="og-image" />
+        </div>
+        {/if}
+        {#if isImageGenerating}
+        <div class="mt-8">
+          <div class="w-full text-gray-900   text-lg font-medium py-3.5 rounded">
+            Generating Image...
+            <div class="mt-5 w-full bg-gray-200 rounded-full h-3 dark:bg-gray-300">
+              <div class="bg-gray-900 h-3 rounded-full" style="width: 0%; animation: loading 3s forwards;"></div>
+            </div>
+          </div>
+        </div>
+        {/if}
         <div class="mt-8">
           <button
-            class="w-full text-gray-900 bg-[#ffc480] border-[3px] border-gray-900 text-lg font-medium py-3.5 rounded"
+            on:click={generateImage}
+            disabled={isImageGenerating}
+            class="w-full text-gray-900 bg-[#ffc480] border-[3px] border-gray-900 text-lg font-medium py-3.5 rounded disabled:bg-slate-400"
           >
             Generate Image
           </button>
@@ -316,10 +483,10 @@ $:previewHeight = ogImageTemplateWrapper ? ogImageTemplateWrapper.offsetWidth  :
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div
-        class="{selectedTemplate === template ? 'border-[3px] border-[#5cf1a4]' : ''} cursor-pointer relative w-fit"
+        class="{selectedTemplate === template ? 'border-[3px] border-[#5cf1a4]' : ''} cursor-pointer relative w-fit "
         on:click={() => selectedTemplate = template}
       >
-      <div class="absolute inset-0 z-0">
+      <div class="absolute inset-0" style="z-index: 1;">
       {#if selectedTemplate === template}
         <div class="m-2">
           ✅
