a20296bb74d3de7a834dea6887be8e13860d4576
suyash-thakur <500067151@stu.upes.ac.in>
2024-08-03 16:37:57 +0530

    update sitemap

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -2,8 +2,7 @@ import { getBlogLinks } from '../../api/blog';
 
 export async function GET() {
 	const response = await getBlogLinks();
-	const { links } = response;
-	console.log(links);
+   const { links } = response;
 	const urls = links.map((link) => {
 		return `
          <url>
@@ -31,6 +30,12 @@ export async function GET() {
           <changefreq>daily</changefreq>
           <priority>0.8</priority>
         </url>
+        <url>
+            <loc>https://pictify.io/pricing</loc>
+            <lastmod>2024-08-02</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>0.8</priority>
+         </url>
          <url>
             <loc>https://pictify.io/blogs</loc>
             <lastmod>2024-04-19</lastmod>
